a4aa492322c7b83d62bd272fb7c55c9d0c4b286a
Abdullah Durrani <Abdullahkd31@gmail.com>
2023-12-09 13:27:40 -0500

    updated project, completed step 11 of spltting data in to three different lists: customer, sales and thread_sold.

--- Python/Codecademy/Projects/Thread Shed.py
+++ Python/Codecademy/Projects/Thread Shed.py
@@ -1,177 +1,136 @@
 daily_sales = \
-"""Edith Mcbride   ;,;$1.21   ;,;   white ;,; 
-09/15/17   ,Herbert Tran   ;,;   $7.29;,; 
-white&blue;,;   09/15/17 ,Paul Clarke ;,;$12.52 
-;,;   white&blue ;,; 09/15/17 ,Lucille Caldwell   
-;,;   $5.13   ;,; white   ;,; 09/15/17,
-Eduardo George   ;,;$20.39;,; white&yellow 
-;,;09/15/17   ,   Danny Mclaughlin;,;$30.82;,;   
-purple ;,;09/15/17 ,Stacy Vargas;,; $1.85   ;,; 
-purple&yellow ;,;09/15/17,   Shaun Brock;,; 
-$17.98;,;purple&yellow ;,; 09/15/17 , 
-Erick Harper ;,;$17.41;,; blue ;,; 09/15/17, 
-Michelle Howell ;,;$28.59;,; blue;,;   09/15/17   , 
-Carroll Boyd;,; $14.51;,;   purple&blue   ;,;   
-09/15/17   , Teresa Carter   ;,; $19.64 ;,; 
-white;,;09/15/17   ,   Jacob Kennedy ;,; $11.40   
-;,; white&red   ;,; 09/15/17, Craig Chambers;,; 
-$8.79 ;,; white&blue&red   ;,;09/15/17   , Peggy Bell;,; $8.65 ;,;blue   ;,; 09/15/17,   Kenneth Cunningham ;,;   $10.53;,;   green&blue   ;,; 
-09/15/17   ,   Marvin Morgan;,;   $16.49;,; 
-green&blue&red   ;,;   09/15/17 ,Marjorie Russell 
-;,; $6.55 ;,;   green&blue&red;,;   09/15/17 ,
-Israel Cummings;,;   $11.86   ;,;black;,;  
-09/15/17,   June Doyle   ;,;   $22.29 ;,;  
-black&yellow ;,;09/15/17 , Jaime Buchanan   ;,;   
-$8.35;,;   white&black&yellow   ;,;   09/15/17,   
-Rhonda Farmer;,;$2.91 ;,;   white&black&yellow   
-;,;09/15/17, Darren Mckenzie ;,;$22.94;,;green 
-;,;09/15/17,Rufus Malone;,;$4.70   ;,; green&yellow 
-;,; 09/15/17   ,Hubert Miles;,;   $3.59   
-;,;green&yellow&blue;,;   09/15/17   , Joseph Bridges  ;,;$5.66   ;,; green&yellow&purple&blue 
-;,;   09/15/17 , Sergio Murphy   ;,;$17.51   ;,;   
-black   ;,;   09/15/17 , Audrey Ferguson ;,; 
-$5.54;,;black&blue   ;,;09/15/17 ,Edna Williams ;,; 
-$17.13;,; black&blue;,;   09/15/17,   Randy Fleming;,;   $21.13 ;,;black ;,;09/15/17 ,Elisa Hart;,; $0.35   ;,; black&purple;,;   09/15/17   ,
-Ernesto Hunt ;,; $13.91   ;,;   black&purple ;,;   
-09/15/17,   Shannon Chavez   ;,;$19.26   ;,; 
-yellow;,; 09/15/17   , Sammy Cain;,; $5.45;,;   
-yellow&red ;,;09/15/17 ,   Steven Reeves ;,;$5.50   
-;,;   yellow;,;   09/15/17, Ruben Jones   ;,; 
-$14.56 ;,;   yellow&blue;,;09/15/17 , Essie Hansen;,;   $7.33   ;,;   yellow&blue&red
-;,; 09/15/17   ,   Rene Hardy   ;,; $20.22   ;,; 
-black ;,;   09/15/17 ,   Lucy Snyder   ;,; $8.67   
-;,;black&red  ;,; 09/15/17 ,Dallas Obrien ;,;   
-$8.31;,;   black&red ;,;   09/15/17,   Stacey Payne 
-;,;   $15.70   ;,;   white&black&red ;,;09/15/17   
-,   Tanya Cox   ;,;   $6.74   ;,;yellow   ;,; 
-09/15/17 , Melody Moran ;,;   $30.84   
-;,;yellow&black;,;   09/15/17 , Louise Becker   ;,; 
-$12.31 ;,; green&yellow&black;,;   09/15/17 ,
-Ryan Webster;,;$2.94 ;,; yellow ;,; 09/15/17 
-,Justin Blake ;,; $22.46   ;,;white&yellow ;,;   
-09/15/17,   Beverly Baldwin ;,;   $6.60;,;   
-white&yellow&black ;,;09/15/17   ,   Dale Brady   
-;,;   $6.27 ;,; yellow   ;,;09/15/17 ,Guadalupe Potter ;,;$21.12   ;,; yellow;,; 09/15/17   , 
-Desiree Butler ;,;$2.10   ;,;white;,; 09/15/17  
-,Sonja Barnett ;,; $14.22 ;,;white&black;,;   
-09/15/17, Angelica Garza;,;$11.60;,;white&black   
-;,;   09/15/17   ,   Jamie Welch   ;,; $25.27   ;,; 
-white&black&red ;,;09/15/17   ,   Rex Hudson   
-;,;$8.26;,;   purple;,; 09/15/17 ,   Nadine Gibbs 
-;,;   $30.80 ;,;   purple&yellow   ;,; 09/15/17   , 
-Hannah Pratt;,;   $22.61   ;,;   purple&yellow   
-;,;09/15/17,Gayle Richards;,;$22.19 ;,; 
-green&purple&yellow ;,;09/15/17   ,Stanley Holland 
-;,; $7.47   ;,; red ;,; 09/15/17 , Anna Dean;,;$5.49 ;,; yellow&red ;,;   09/15/17   ,
-Terrance Saunders ;,;   $23.70  ;,;green&yellow&red 
-;,; 09/15/17 ,   Brandi Zimmerman ;,; $26.66 ;,; 
-red   ;,;09/15/17 ,Guadalupe Freeman ;,; $25.95;,; 
-green&red ;,;   09/15/17   ,Irving Patterson 
-;,;$19.55 ;,; green&white&red ;,;   09/15/17 ,Karl Ross;,;   $15.68;,;   white ;,;   09/15/17 , Brandy Cortez ;,;$23.57;,;   white&red   ;,;09/15/17, 
-Mamie Riley   ;,;$29.32;,; purple;,;09/15/17 ,Mike Thornton   ;,; $26.44 ;,;   purple   ;,; 09/15/17, 
-Jamie Vaughn   ;,; $17.24;,;green ;,; 09/15/17   , 
-Noah Day ;,;   $8.49   ;,;green   ;,;09/15/17   
-,Josephine Keller ;,;$13.10 ;,;green;,;   09/15/17 ,   Tracey Wolfe;,;$20.39 ;,; red   ;,; 09/15/17 ,
-Ignacio Parks;,;$14.70   ;,; white&red ;,;09/15/17 
-, Beatrice Newman ;,;$22.45   ;,;white&purple&red 
-;,;   09/15/17, Andre Norris   ;,;   $28.46   ;,;   
-red;,;   09/15/17 ,   Albert Lewis ;,; $23.89;,;   
-black&red;,; 09/15/17,   Javier Bailey   ;,;   
-$24.49   ;,; black&red ;,; 09/15/17   , Everett Lyons ;,;$1.81;,;   black&red ;,; 09/15/17 ,   
-Abraham Maxwell;,; $6.81   ;,;green;,;   09/15/17   
-,   Traci Craig ;,;$0.65;,; green&yellow;,; 
-09/15/17 , Jeffrey Jenkins   ;,;$26.45;,; 
-green&yellow&blue   ;,;   09/15/17,   Merle Wilson 
-;,;   $7.69 ;,; purple;,; 09/15/17,Janis Franklin   
-;,;$8.74   ;,; purple&black   ;,;09/15/17 ,  
-Leonard Guerrero ;,;   $1.86   ;,;yellow  
-;,;09/15/17,Lana Sanchez;,;$14.75   ;,; yellow;,;   
-09/15/17   ,Donna Ball ;,; $28.10  ;,; 
-yellow&blue;,;   09/15/17   , Terrell Barber   ;,; 
-$9.91   ;,; green ;,;09/15/17   ,Jody Flores;,; 
-$16.34 ;,; green ;,;   09/15/17,   Daryl Herrera 
-;,;$27.57;,; white;,;   09/15/17   , Miguel Mcguire;,;$5.25;,; white&blue   ;,;   09/15/17 ,   
-Rogelio Gonzalez;,; $9.51;,;   white&black&blue   
-;,;   09/15/17   ,   Lora Hammond ;,;$20.56 ;,; 
-green;,;   09/15/17,Owen Ward;,; $21.64   ;,;   
-green&yellow;,;09/15/17,Malcolm Morales ;,;   
-$24.99   ;,;   green&yellow&black;,; 09/15/17 ,   
-Eric Mcdaniel ;,;$29.70;,; green ;,; 09/15/17 
-,Madeline Estrada;,;   $15.52;,;green;,;   09/15/17 
-, Leticia Manning;,;$15.70 ;,; green&purple;,; 
-09/15/17 ,   Mario Wallace ;,; $12.36 ;,;green ;,; 
-09/15/17,Lewis Glover;,;   $13.66   ;,;   
-green&white;,;09/15/17,   Gail Phelps   ;,;$30.52   
-;,; green&white&blue   ;,; 09/15/17 , Myrtle Morris 
-;,;   $22.66   ;,; green&white&blue;,;09/15/17"""
+    """Edith Mcbride   ;,;$1.21   ;,;   white ;,; 
+    09/15/17   ,Herbert Tran   ;,;   $7.29;,; 
+    white&blue;,;   09/15/17 ,Paul Clarke ;,;$12.52 
+    ;,;   white&blue ;,; 09/15/17 ,Lucille Caldwell   
+    ;,;   $5.13   ;,; white   ;,; 09/15/17,
+    Eduardo George   ;,;$20.39;,; white&yellow 
+    ;,;09/15/17   ,   Danny Mclaughlin;,;$30.82;,;   
+    purple ;,;09/15/17 ,Stacy Vargas;,; $1.85   ;,; 
+    purple&yellow ;,;09/15/17,   Shaun Brock;,; 
+    $17.98;,;purple&yellow ;,; 09/15/17 , 
+    Erick Harper ;,;$17.41;,; blue ;,; 09/15/17, 
+    Michelle Howell ;,;$28.59;,; blue;,;   09/15/17   , 
+    Carroll Boyd;,; $14.51;,;   purple&blue   ;,;   
+    09/15/17   , Teresa Carter   ;,; $19.64 ;,; 
+    white;,;09/15/17   ,   Jacob Kennedy ;,; $11.40   
+    ;,; white&red   ;,; 09/15/17, Craig Chambers;,; 
+    $8.79 ;,; white&blue&red   ;,;09/15/17   , Peggy Bell;,; $8.65 ;,;blue   ;,; 09/15/17,   Kenneth Cunningham ;,;   $10.53;,;   green&blue   ;,; 
+    09/15/17   ,   Marvin Morgan;,;   $16.49;,; 
+    green&blue&red   ;,;   09/15/17 ,Marjorie Russell 
+    ;,; $6.55 ;,;   green&blue&red;,;   09/15/17 ,
+    Israel Cummings;,;   $11.86   ;,;black;,;  
+    09/15/17,   June Doyle   ;,;   $22.29 ;,;  
+    black&yellow ;,;09/15/17 , Jaime Buchanan   ;,;   
+    $8.35;,;   white&black&yellow   ;,;   09/15/17,   
+    Rhonda Farmer;,;$2.91 ;,;   white&black&yellow   
+    ;,;09/15/17, Darren Mckenzie ;,;$22.94;,;green 
+    ;,;09/15/17,Rufus Malone;,;$4.70   ;,; green&yellow 
+    ;,; 09/15/17   ,Hubert Miles;,;   $3.59   
+    ;,;green&yellow&blue;,;   09/15/17   , Joseph Bridges  ;,;$5.66   ;,; green&yellow&purple&blue 
+    ;,;   09/15/17 , Sergio Murphy   ;,;$17.51   ;,;   
+    black   ;,;   09/15/17 , Audrey Ferguson ;,; 
+    $5.54;,;black&blue   ;,;09/15/17 ,Edna Williams ;,; 
+    $17.13;,; black&blue;,;   09/15/17,   Randy Fleming;,;   $21.13 ;,;black ;,;09/15/17 ,Elisa Hart;,; $0.35   ;,; black&purple;,;   09/15/17   ,
+    Ernesto Hunt ;,; $13.91   ;,;   black&purple ;,;   
+    09/15/17,   Shannon Chavez   ;,;$19.26   ;,; 
+    yellow;,; 09/15/17   , Sammy Cain;,; $5.45;,;   
+    yellow&red ;,;09/15/17 ,   Steven Reeves ;,;$5.50   
+    ;,;   yellow;,;   09/15/17, Ruben Jones   ;,; 
+    $14.56 ;,;   yellow&blue;,;09/15/17 , Essie Hansen;,;   $7.33   ;,;   yellow&blue&red
+    ;,; 09/15/17   ,   Rene Hardy   ;,; $20.22   ;,; 
+    black ;,;   09/15/17 ,   Lucy Snyder   ;,; $8.67   
+    ;,;black&red  ;,; 09/15/17 ,Dallas Obrien ;,;   
+    $8.31;,;   black&red ;,;   09/15/17,   Stacey Payne 
+    ;,;   $15.70   ;,;   white&black&red ;,;09/15/17   
+    ,   Tanya Cox   ;,;   $6.74   ;,;yellow   ;,; 
+    09/15/17 , Melody Moran ;,;   $30.84   
+    ;,;yellow&black;,;   09/15/17 , Louise Becker   ;,; 
+    $12.31 ;,; green&yellow&black;,;   09/15/17 ,
+    Ryan Webster;,;$2.94 ;,; yellow ;,; 09/15/17 
+    ,Justin Blake ;,; $22.46   ;,;white&yellow ;,;   
+    09/15/17,   Beverly Baldwin ;,;   $6.60;,;   
+    white&yellow&black ;,;09/15/17   ,   Dale Brady   
+    ;,;   $6.27 ;,; yellow   ;,;09/15/17 ,Guadalupe Potter ;,;$21.12   ;,; yellow;,; 09/15/17   , 
+    Desiree Butler ;,;$2.10   ;,;white;,; 09/15/17  
+    ,Sonja Barnett ;,; $14.22 ;,;white&black;,;   
+    09/15/17, Angelica Garza;,;$11.60;,;white&black   
+    ;,;   09/15/17   ,   Jamie Welch   ;,; $25.27   ;,; 
+    white&black&red ;,;09/15/17   ,   Rex Hudson   
+    ;,;$8.26;,;   purple;,; 09/15/17 ,   Nadine Gibbs 
+    ;,;   $30.80 ;,;   purple&yellow   ;,; 09/15/17   , 
+    Hannah Pratt;,;   $22.61   ;,;   purple&yellow   
+    ;,;09/15/17,Gayle Richards;,;$22.19 ;,; 
+    green&purple&yellow ;,;09/15/17   ,Stanley Holland 
+    ;,; $7.47   ;,; red ;,; 09/15/17 , Anna Dean;,;$5.49 ;,; yellow&red ;,;   09/15/17   ,
+    Terrance Saunders ;,;   $23.70  ;,;green&yellow&red 
+    ;,; 09/15/17 ,   Brandi Zimmerman ;,; $26.66 ;,; 
+    red   ;,;09/15/17 ,Guadalupe Freeman ;,; $25.95;,; 
+    green&red ;,;   09/15/17   ,Irving Patterson 
+    ;,;$19.55 ;,; green&white&red ;,;   09/15/17 ,Karl Ross;,;   $15.68;,;   white ;,;   09/15/17 , Brandy Cortez ;,;$23.57;,;   white&red   ;,;09/15/17, 
+    Mamie Riley   ;,;$29.32;,; purple;,;09/15/17 ,Mike Thornton   ;,; $26.44 ;,;   purple   ;,; 09/15/17, 
+    Jamie Vaughn   ;,; $17.24;,;green ;,; 09/15/17   , 
+    Noah Day ;,;   $8.49   ;,;green   ;,;09/15/17   
+    ,Josephine Keller ;,;$13.10 ;,;green;,;   09/15/17 ,   Tracey Wolfe;,;$20.39 ;,; red   ;,; 09/15/17 ,
+    Ignacio Parks;,;$14.70   ;,; white&red ;,;09/15/17 
+    , Beatrice Newman ;,;$22.45   ;,;white&purple&red 
+    ;,;   09/15/17, Andre Norris   ;,;   $28.46   ;,;   
+    red;,;   09/15/17 ,   Albert Lewis ;,; $23.89;,;   
+    black&red;,; 09/15/17,   Javier Bailey   ;,;   
+    $24.49   ;,; black&red ;,; 09/15/17   , Everett Lyons ;,;$1.81;,;   black&red ;,; 09/15/17 ,   
+    Abraham Maxwell;,; $6.81   ;,;green;,;   09/15/17   
+    ,   Traci Craig ;,;$0.65;,; green&yellow;,; 
+    09/15/17 , Jeffrey Jenkins   ;,;$26.45;,; 
+    green&yellow&blue   ;,;   09/15/17,   Merle Wilson 
+    ;,;   $7.69 ;,; purple;,; 09/15/17,Janis Franklin   
+    ;,;$8.74   ;,; purple&black   ;,;09/15/17 ,  
+    Leonard Guerrero ;,;   $1.86   ;,;yellow  
+    ;,;09/15/17,Lana Sanchez;,;$14.75   ;,; yellow;,;   
+    09/15/17   ,Donna Ball ;,; $28.10  ;,; 
+    yellow&blue;,;   09/15/17   , Terrell Barber   ;,; 
+    $9.91   ;,; green ;,;09/15/17   ,Jody Flores;,; 
+    $16.34 ;,; green ;,;   09/15/17,   Daryl Herrera 
+    ;,;$27.57;,; white;,;   09/15/17   , Miguel Mcguire;,;$5.25;,; white&blue   ;,;   09/15/17 ,   
+    Rogelio Gonzalez;,; $9.51;,;   white&black&blue   
+    ;,;   09/15/17   ,   Lora Hammond ;,;$20.56 ;,; 
+    green;,;   09/15/17,Owen Ward;,; $21.64   ;,;   
+    green&yellow;,;09/15/17,Malcolm Morales ;,;   
+    $24.99   ;,;   green&yellow&black;,; 09/15/17 ,   
+    Eric Mcdaniel ;,;$29.70;,; green ;,; 09/15/17 
+    ,Madeline Estrada;,;   $15.52;,;green;,;   09/15/17 
+    , Leticia Manning;,;$15.70 ;,; green&purple;,; 
+    09/15/17 ,   Mario Wallace ;,; $12.36 ;,;green ;,; 
+    09/15/17,Lewis Glover;,;   $13.66   ;,;   
+    green&white;,;09/15/17,   Gail Phelps   ;,;$30.52   
+    ;,; green&white&blue   ;,; 09/15/17 , Myrtle Morris 
+    ;,;   $22.66   ;,; green&white&blue;,;09/15/17"""
 
-#------------------------------------------------
+# ------------------------------------------------
 # Start coding below!
+print("DAILY SALES, ORIGINAL LIST:")
+print()
+print(daily_sales)
+daily_sales_replaced = daily_sales.replace(";,;", "^")
 
-'''First attempt at stripping varaible, doing striaght .strip() function, 
-not working'''
-
-# daily_sales_stripped = daily_sales.strip()
-# print()
-# print("Stripped: " + str(daily_sales_stripped))
-
-# create empty list
-# daily_sales_stripped = []
-
-# iterate through list to clean or "strip" list:
-# for lists in daily_sales:
-#     string_stripped = lists.strip(' ')
-#     daily_sales_stripped.append(string_stripped)
-#
-# print()
-# print("Daily Sales Stripped" + str(daily_sales_stripped))
-
-'''Codecademy Instructions Follow'''
-#------------------------------------------------
-# Start coding below!
-
-daily_sales_replaced = daily_sales.replace(";,;", "x")
-# print(daily_sales_replaced)
+print()
+print()
+print()
+print("DAILY SALES REPLACED with \"^\" :")
+print()
+print(daily_sales_replaced)
 
 daily_transactions = daily_sales_replaced.split(",")
 
-# print()
-# print("Daily Transactions: " + str(daily_transactions))
+print()
+print()
+print()
+print("DAILY TRANSACTIONS (TURNED STRING INTO LIST):")
+print()
+print(daily_transactions)
 
 daily_transactions_split = []
 
-daily_transactions_split = [items.split("x") for items in daily_transactions]
-
-# ----------------- Example of code, attempting to see the difference caused by each line of code: ------------------#
-
-'''
-initial data example: 
-
-"""Edith Mcbride   ;,;$1.21   ;,;   white ;,; 
-09/15/17   ,
-
-Step 1:
-Replacing ";,;" with "x"
-daily_sales_replaced = daily_sales.replace(";,;", "x")
-print(daily_sales_replaced):
-
-output 1:
-Edith Mcbride   x$1.21   x   white x 
-09/15/17   ,  
-
-Step 2:
-splitting the data at the ","
-daily_transactions = daily_sales_replaced.split(",")
-print()
-print("Daily Transactions: " + str(daily_transactions))
-
-output 2:
-['Edith Mcbride   x$1.21   x   white x \n09/15/17   ',
-
-This has turned the initial string into a list!! Very important!
-
-'''
-
+daily_transactions_split = [items.split("^") for items in daily_transactions]
 
 # 7.
 # print(daily_transactions_split)
@@ -184,8 +143,59 @@ transactions_clean = [values.strip() for transactions in daily_transactions_spli
 # 9.
 print()
 print()
+print()
+print("TRANSACTIONS CLEAN (USED STRIP METHOD TO REMOVE ANY REMAINIG WHITE SPACE):")
+print()
 print(transactions_clean)
 
+# 10.
+customers = []
+sales = []
+thread_sold = []
+
+# 11.
+for i in range(0, len(transactions_clean), 4):
+    customers.append(transactions_clean[i])
+
+# The previous iteration of the code (line 157) yielded unexpected outcomes in the 'for loop'. Instead of isolating only 'names', it also included 'amount' and 'thread_sold' data. The root of the issue was traced back to the use of 'x' as a replacement for the ';,;' sequence. This approach inadvertently affected the data integrity, as some 'names' contained the letter 'x', disrupting the intended pattern of selecting every fourth element. During the string processing, the .split(x) method inadvertently generated empty strings, leading to a miscount. This prompted the following debugging process
+
+# searching for string, with string.find(substring)
+
+# - -- - First Attempt, not going to work, list of strings, not string - -- -- #
+# customer_index = transactions_clean.find("Tanya Co")
+# print(customer_index)
+
+# - -- - Second Attempt - -- -#
+# customer_index = -1
+# for i, transaction in enumerate(transactions_clean):
+#   if "Tanya Co" in transaction
+
+# ---- I reached a point where I needed assistance, so I involved Malik in the troubleshooting process. After explaining the code and the project's progress, Malik proposed using the Ctrl+F find function in the editor, instead of relying on the .find string method in Python. This approach led to the discovery that the element 'Tanya Co' — which was the ending point of the the 'for loop' in yeilding expected values ("names") — was originally 'Tanya Cox'. The initial use of .split(x) had split the string at 'x', inadvertently creating an unaccounted-for empty string in the process.
+
+
+# 11b
+for i in range(1, len(transactions_clean), 4):
+    sales.append(transactions_clean[i])
+
+# 11c
+for i in range(2, len(transactions_clean), 4):
+    thread_sold.append(transactions_clean[i])
+
+print()
+print()
+print("LIST OF CUSTOMERS:")
+print()
+print(customers)
+
+print()
+print()
+print("LIST OF SALES AMOUNT:")
+print(sales)
+
+print()
+print()
+print("LIST OF TYPE OF THREAD SOLD:")
+print(thread_sold)
 
 
 
