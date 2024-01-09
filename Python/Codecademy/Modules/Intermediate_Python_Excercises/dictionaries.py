903fd3211a838209fce601c514735c76f68fbf2e
Abdullah Durrani <abdullahkd31@gmail.com>
2024-01-09 15:00:05 -0500

    added new excercise in dictionaries.py

--- Python/Codecademy/Modules/Intermediate_Python_Excercises/dictionaries.py
+++ Python/Codecademy/Modules/Intermediate_Python_Excercises/dictionaries.py
@@ -20,7 +20,18 @@ library = {"The Best Songs": plays, "Sunday Feelings": {}}
 print()
 print(library)
 
+# Delete a Key Excercise
+print()
+print()
+print("Delete a Key Excercise:")
 
+available_items = {"health potion": 10, "cake of the cure": 5, "green elixir": 20, "strength sandwich": 25, "stamina grains": 15, "power stew": 30}
+health_points = 20
+
+# 1.
+for items, value:
+  if "stamina grains" > 0:
+  "stamina grains" + health_points
 
 
 
