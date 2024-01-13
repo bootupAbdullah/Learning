63a1da9cf52c7cfbea2278b6ee917804e878903d
Abdullah Durrani <Abdullahkd31@gmail.com>
2024-01-13 08:44:48 -0500

    updated project to #9, adding player dictonary and corresponding words

---
+++ Python/Codecademy/Projects/Scarbble.py
@@ -0,0 +1,39 @@
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+           "W", "X", "Y", "Z"]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+
+# 1.
+letter_to_points = {key: value for key, value in zip(letters, points)}
+
+# print(letter_to_points)
+
+# 2.
+letter_to_points[" "] = 0
+print(letter_to_points)
+
+
+# 3. -  6.
+def score_word(word):
+    point_total = 0
+    word_uppercase = word.upper()
+    for letter in word_uppercase:
+        if letter in letter_to_points.keys():
+            point_total += letter_to_points[letter]
+        else:
+            point_total += 0
+    return point_total
+
+# 7. - 8.
+brownie_points = score_word("brownie")
+print(brownie_points)
+
+# 9.
+player_to_words = {"player1":["blue", "tennis", "exit"], "wordNerd":["EARTH", "EYES", "MACHINE"],
+"Lexi Con":["ERASER", "BELLY", "HUSKY"], "Prof Reader":["zap", "coma", "period"]}
+
+#
+
+
+
+
+
