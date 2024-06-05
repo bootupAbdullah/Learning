846524e884a756bc75582a89d8752f1bf2fc8aee
Abdullah Durrani <Abdullahkd31@gmail.com>
2024-06-05 16:51:12 -0400

    working on code challenge, how to ignore duplicates.

--- Python/Codecademy/Python Code Challenges/Code Challenges II/Python Code Challenges Strings.py
+++ Python/Codecademy/Python Code Challenges/Code Challenges II/Python Code Challenges Strings.py
@@ -6,19 +6,27 @@
 letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
 def  unique_english_letters(word):
-   all_letters = 0
-   upper_case_letters = 0
-   lower_case_letters = 0
-   for letters in word:
-    if letters.lower():
-      lower_case_letters += 1
-    if letters.upper():
-      upper_case_letters += 1
-    all_letters = upper_case_letters + lower_case_letters
-    return all_letters
+  letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  count = 0
+  for word in letters:
+    if letters in word:
+      count += 1
+  return count
+ 
+  
+  #  all_letters = 0
+  #  upper_case_letters = 0
+  #  lower_case_letters = 0
+  #  for letters in word:
+  #   if letters == letters.lower():
+  #     lower_case_letters += 1
+  #   if letters == letters.upper():
+  #     upper_case_letters += 1
+  #     all_letters = upper_case_letters + lower_case_letters
+  #  return ("\n" + "Lower Case Letters:" + str(lower_case_letters) + "\n" + "\nUpper Case Letters:" + str(upper_case_letters) + "\n" + "\nAll Letters:" + str(all_letters))
 
 
-print(unique_english_letters("HeLlO"))
+print(unique_english_letters("HellO"))
    
 
 
