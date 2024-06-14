53577fa8d5e8fcd6d29b445f99ec4c3be6ec43c5
Abdullah Durrani <Abdullahkd31@gmail.com>
2024-06-14 19:23:59 -0400

    workinng on challenge 3'
    q
    '

---
+++ Python/Codecademy/Python Code Challenges/Code Challenges II/3. Count Multi X.py
@@ -0,0 +1,19 @@
+# Write your count_multi_char_x function here:
+# def count_multi_char_x(word, x):
+#   counter = 0
+#   delimiter = []
+#   for letters in word:
+#     delimiter.append(x)
+#     if delimiter in word:
+#       counter += 1
+#     return counter
+
+def count_multu_char_x(word, x):
+  
+
+
+# Uncomment these function calls to test your function:
+print(count_multi_char_x("mississippi", "iss"))
+# should print 2
+#print(count_multi_char_x("apple", "pp"))
+# should print 1
