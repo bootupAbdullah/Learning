80ab9f33b1488a6a414003cc8fb05498689562bc
Abdullah Durrani <abdullahkd31@gmail.com>
2024-07-26 22:58:13 -0400

    javascript tutorial, just started work on variables

--- Javascript/Javascript Tutorial/app.js
+++ Javascript/Javascript Tutorial/app.js
@@ -1,3 +1,5 @@
+"user strict";
+
 console.log("hello");
 console.log("I am writing in Javascript");
 
@@ -5,4 +7,6 @@ console.log("I am writing in Javascript");
 //testing some scripts:
 //<script> alert("Hey, this is Javascript! Look you\'re already doing it! :"))</script>
 // TIL: you don't need the 'script' tag ie <script> on a seperate file that has been attached to the html through 'src='
-// alert("I'm JavaScript!")
+alert("I'm JavaScript!")
+
+
