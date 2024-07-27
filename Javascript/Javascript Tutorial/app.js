2ba3515040fb881126082761a9c879482f7cf1fa
Abdulla Durrani <Abdullahkd@gmail.com>
2024-07-27 14:50:39 -0400

    worked on variables and types

--- Javascript/Javascript Tutorial/app.js
+++ Javascript/Javascript Tutorial/app.js
@@ -10,3 +10,26 @@ console.log("I am writing in Javascript");
 alert("I'm JavaScript!")
 
 
+// variables
+// let admin, user;
+
+// user = "John"
+
+// admin = user;
+
+// alert(admin);
+
+// let earth, currentUser;
+
+// const BIRTHDAY = '16.12.1987';
+// const age = someCode(BIRTHDAY);
+
+// types
+
+let user = "Ilya";
+
+alert( `hello ${1}` );
+
+alert( `hello ${"user"}`);
+
+alert( `hello ${user}` );
