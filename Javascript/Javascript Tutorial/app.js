ecf3236a872ba29a40c859904873cfd67c735e58
Abdullah Durrani <abdullahkd31@gmail.com>
2024-07-26 22:09:33 -0400

    javascript tutorials via javascript.info, completed 'alerts' task.

---
+++ Javascript/Javascript Tutorial/app.js
@@ -0,0 +1,8 @@
+console.log("hello");
+console.log("I am writing in Javascript");
+
+
+//testing some scripts:
+//<script> alert("Hey, this is Javascript! Look you\'re already doing it! :"))</script>
+// TIL: you don't need the 'script' tag ie <script> on a seperate file that has been attached to the html through 'src='
+// alert("I'm JavaScript!")
